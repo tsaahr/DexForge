abfbdade0d54ec6482261db81ee63cf7e615859d
tsaahr <lithy.10@hotmail.com>
2025-05-08 15:09:17 -0300

    feat: new db postgresql

--- db/migrate/20250507160256_add_stats_to_pokemons.rb
+++
@@ -1,5 +0,0 @@
-class AddStatsToPokemons < ActiveRecord::Migration[7.1]
-  def change
-    add_column :pokemons, :stats, :text
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_07_160256) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_07_152752) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "pokemons", force: :cascade do |t|
     t.string "name"
     t.string "image_url"
     t.integer "pokeapi_id"
+    t.jsonb "stats", default: []
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "stats"
     t.index ["pokeapi_id"], name: "index_pokemons_on_pokeapi_id"
   end
 
