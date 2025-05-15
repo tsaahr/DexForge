ac61b0de216cb9c9897699cf8c17707242a6589b
tsaahr <lithy.10@hotmail.com>
2025-05-15 19:30:21 -0300

    feat: otimization controller capture

--- app/controllers/capture_controller.rb
+++ app/controllers/capture_controller.rb
@@ -10,36 +10,20 @@ class CaptureController < ApplicationController
     base_pokemon = Pokemon.where(evolves_from: nil, is_legendary: false, is_mythical: false).sample
     evolved = rand < 0.2 ? base_pokemon.auto_evolve_for_level!(wild_level) : base_pokemon
   
-  
-    best_moves = fetch_best_moves(evolved, wild_level)
-    scaled_stats = extract_scaled_stats(evolved, wild_level)
-
-    @wild_pokemon = WildPokemon.create!(
-      pokemon: evolved,
-      level: wild_level,
-      hp: scaled_stats["hp"],
-      current_hp: scaled_stats["hp"],
-      attack: scaled_stats["attack"],
-      defense: scaled_stats["defense"],
-      sp_attack: scaled_stats["special-attack"],
-      sp_defense: scaled_stats["special-defense"],
-      speed: scaled_stats["speed"],
-      moves: best_moves
-    )    
-    
+    @wild_pokemon = WildPokemon.build_with_best_moves(pokemon: evolved, level: wild_level)
+    @wild_pokemon.save!
   end
-
+  
+  
   def show
     @wild_pokemon = WildPokemon.find(params[:id])
-    @all_moves = fetch_best_moves_for_frontend(@wild_pokemon.pokemon)
+    @all_moves = @wild_pokemon.all_possible_moves
   end
   def battle
     @wild_pokemon = WildPokemon.find(params[:id])
-    @all_moves = fetch_best_moves(@wild_pokemon.pokemon, @wild_pokemon.level)
+    @all_moves = @wild_pokemon.all_possible_moves
   end
   
-  
-
   def try_capture
     wild_pokemon = WildPokemon.find(params[:id])
   
@@ -56,59 +40,4 @@ class CaptureController < ApplicationController
       render json: { captured: false }
     end
   end
-  
-
-  private
-
-    
-  def fetch_best_moves(pokemon, level)
-    pokemon_moves = pokemon.pokemon_moves.includes(:move)
-  
-    return [] if pokemon_moves.blank?
-  
-    available_moves = pokemon_moves.select do |pokemon_move|
-      pokemon_move.level_learned_at.nil? || pokemon_move.level_learned_at <= level
-    end
-
-    move_objects = available_moves.map do |pokemon_move|
-      move = pokemon_move.move
-      {
-        name: move.name,
-        power: move.power,
-        accuracy: move.accuracy,
-        pp: move.pp,
-        move_type: move.move_type,
-        damage_class: move.damage_class,
-        description: move.description
-      }
-    end
-    move_objects.sort_by { |m| -(m[:power] || 0) }.first(4)
-  end
-  
-  
-  def extract_scaled_stats(pokemon, level)
-    stats = {
-      "hp" => 0,
-      "attack" => 0,
-      "defense" => 0,
-      "special-attack" => 0,
-      "special-defense" => 0,
-      "speed" => 0
-    }
-  
-    pokemon.stats.each do |s|
-      name = s["stat"]["name"]
-      base = s["base_stat"]
-  
-      if name == "hp"
-        stats["hp"] = (((2 * base) * level) / 100) + level + 10
-      else
-        stats[name] = (((2 * base) * level) / 100) + 5
-      end
-    end
-  
-    stats
-  end
-  
-  
 end
--- app/models/wild_pokemon.rb
+++ app/models/wild_pokemon.rb
@@ -1,3 +1,63 @@
 class WildPokemon < ApplicationRecord
   belongs_to :pokemon
+
+  def generate_stats_from_base
+    return unless pokemon && level
+
+    stats = {}
+    pokemon.stats.each do |s|
+      name = s["stat"]["name"]
+      base = s["base_stat"]
+
+      value = if name == "hp"
+                (((2 * base) * level) / 100) + level + 10
+              else
+                (((2 * base) * level) / 100) + 5
+              end
+
+      stats[name] = value
+    end
+
+    self.hp         = stats["hp"]
+    self.current_hp = stats["hp"]
+    self.attack     = stats["attack"]
+    self.defense    = stats["defense"]
+    self.sp_attack  = stats["special-attack"]
+    self.sp_defense = stats["special-defense"]
+    self.speed      = stats["speed"]
+  end
+
+  def select_best_moves
+    moves_for_level.first(4)
+  end
+
+  def all_possible_moves
+    moves_for_level
+  end
+
+  def self.build_with_best_moves(pokemon:, level:)
+    wild_pokemon = WildPokemon.new(pokemon: pokemon, level: level)
+    wild_pokemon.moves = wild_pokemon.select_best_moves
+    wild_pokemon.send(:generate_stats_from_base)
+    wild_pokemon
+  end
+
+  private
+
+  def moves_for_level
+    pokemon.pokemon_moves.includes(:move).select do |pm|
+      pm.level_learned_at.nil? || pm.level_learned_at <= level
+    end.map do |pm|
+      move = pm.move
+      {
+        name: move.name,
+        power: move.power,
+        accuracy: move.accuracy,
+        pp: move.pp,
+        move_type: move.move_type,
+        damage_class: move.damage_class,
+        description: move.description
+      }
+    end.sort_by { |m| -(m[:power] || 0) }
+  end
 end
